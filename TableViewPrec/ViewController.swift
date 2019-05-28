db1ee8155b8bd471ff9a3e5a348e3c45dabccf55
Vian-YJLee <Vianney.yjlee@gmail.com>
2019-05-28 21:06:50 +0900

    Initial Commit

---
+++ TableViewPrec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPrec
+//
+//  Created by LeeYongJin on 28/05/2019.
+//  Copyright © 2019 vian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
