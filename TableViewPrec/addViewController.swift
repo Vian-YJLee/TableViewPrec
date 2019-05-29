b3887f849063147319f51236acf770fcf74c61cc
Vian-YJLee <Vianney.yjlee@gmail.com>
2019-05-29 19:30:47 +0900

    "스토리보드 구성, 각 view Controller 파일연결"

---
+++ TableViewPrec/addViewController.swift
@@ -0,0 +1,33 @@
+//
+//  addViewController.swift
+//  TableViewPrec
+//
+//  Created by LeeYongJin on 29/05/2019.
+//  Copyright © 2019 vian. All rights reserved.
+//
+
+import UIKit
+
+class addViewController: UIViewController {
+
+    @IBOutlet var tfAddItem: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnAddItem(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
